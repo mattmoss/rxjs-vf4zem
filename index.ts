2a8a3b745a4159150b01327e0a5bbdbf875e5efb
mattmoss <mossm@objectcomputing.com>
2019-04-10 11:33:50 -0500

    dfs2 and parentAndChildren seems to be what I want

--- index.ts
+++ index.ts
@@ -1,21 +1,27 @@
-import { Observable, of, empty, from } from 'rxjs'; 
-import { map, concatMap, expand, toArray, delay } from 'rxjs/operators';
+;import { Observable, of, empty, from } from 'rxjs'; 
+import { map, concat, concatMap, concatAll, expand, toArray, delay, tap } from 'rxjs/operators';
 
 type Getter = (x: number) => Observable<number[]>;
 
 const http = {
-  1: [8, 9],
-  2: [4],
-  4: [7],
-  9: [22, 23, 24]
+  1: [2, 6],
+  2: [3, 4],
+  4: [5],
+  6: [7, 13, 14],
+  7: [8, 9, 10, 11, 12],
+  14: [15, 16],
+  17: [18],
+  18: [19],
+  20: [21],
 };
+
 const getChildren = (id: number) =>
   (http[id] ? of(http[id] as number[]) : empty());
 
 const getChildrenWithDelay = (id: number) =>
-  getChildren(id).pipe(delay(Math.random() * 500));
+  getChildren(id).pipe(delay(Math.random() * 300));
 
-const rootIds = of([1, 2, 3]);
+const rootIds = of([1, 17, 20]);
 
 const dfs = (getter: Getter) => rootIds.pipe(
   concatMap(ids => from(ids)),
@@ -27,10 +33,40 @@ const dfs = (getter: Getter) => rootIds.pipe(
   toArray()
 );
 
-dfs(getChildren).subscribe(
-  data => console.log(`dfs no delay: ${JSON.stringify(data)}`)
+
+const parentAndChildren = (getChildren: Getter, id: number) =>
+  of(id).pipe(
+    concat(
+      getChildren(id).pipe(
+        concatMap(children => 
+          children.map(child => parentAndChildren(getChildren, child))
+        ),
+        concatAll()
+      )
+    ),
+  );
+
+const dfs2 = (getChildren: Getter, ids: number[]) =>
+  from(ids).pipe(
+    concatMap(id => parentAndChildren(getChildren, id)),
+  );
+
+// parentAndChildren(getChildrenWithDelay, 1).subscribe(data => console.log(data));
+let allIds = [];
+dfs2(getChildrenWithDelay, [1, 17, 20]).subscribe(
+  data => {
+    allIds.push(data);
+    console.log(`Recv: ${data}`);
+  },
+  err => console.error(err),
+  () => console.log(`Complete array: ${allIds}`)
 );
 
-dfs(getChildrenWithDelay).subscribe(
-  data => console.log(`dfs with delay: ${JSON.stringify(data)}`)
-);
+
+// dfs(getChildren).subscribe(
+//   data => console.log('dfs no delay: ' + data)
+// );
+
+// dfs(getChildrenWithDelay).subscribe(
+//   data => console.log('dfs with delay: ' + data)
+// );
